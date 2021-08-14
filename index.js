d03d7d5339abed6bb1111bc71232e630c904e4b9
jackkweyunga <jackkweyunga@gmail.com>
2021-08-14 05:12:06 +0300

    feat(index.js): added ability to fetch data from an api

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const greetMe = (name) => {
-    console.log(`Hi ${name}`);
+// imports
+const { default: axios } = require("axios");
+const { config } = require("./config")
+
+class Nidajs {
+
+    constructor() {
+        this.api = axios.create({
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": "0",
+            },
+        })
+    }
+
+    async apiCall(id) {
+        return await this.api.post(config.url(id));
+    }
+
+    filterData(data) {
+        return data["obj"]["result"];
+    }
+
+
+    //load the citizens data
+    async loadDetails(id) {
+
+        let status;
+        let data;
+
+        let a_call = await this.apiCall(id).then(response => {
+            status = response.status
+            data = this.filterData(response.data)
+        })
+
+        // console.log(data);
+        return data;
+    }
 }
 
-module.exports =  {
-    greetMe
-}
+module.exports = Nidajs
+
--- index.test.js
+++ index.test.js
@@ -1,7 +1,35 @@
 
 
-const { greetMe } = require("./index");
+const Nidajs = require("./index");
 
-test('should return void', () => {
-    expect(greetMe("jek")).toBeUndefined();
-});
+const sample_id = "20000207331010000126";
+
+const nidajs = new Nidajs()
+
+test('should return a response 200 and data ', async () => {
+
+    let status;
+    let data;
+
+    let a_call = await nidajs.apiCall(sample_id).then(response => {
+        status = response.status
+        data = response.data["obj"]["result"]
+        return response;
+    })
+
+    expect(a_call).toBeDefined();
+    expect(status).toEqual(200);
+    expect(data).toBeDefined();
+}, 30000);
+
+test('should return a dictionary with user details', async () => {
+
+    let data;
+    
+    await nidajs.loadDetails(sample_id).then( resp => {
+        data = resp;
+    })
+
+    expect(data).toBeDefined();
+}, 30000)
+
